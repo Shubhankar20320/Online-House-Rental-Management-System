b98d0b8c4fdacbdee1a6125acd1c7c2b313bf23c
Subhankar Mandal <83447635+Shubhankar20320@users.noreply.github.com>
2022-09-26 01:02:50 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,158 @@
+<?php
+	session_start();
+	include 'connection.php';
+	error_reporting(0);
+
+	if(isset($_POST['submit'])){
+		$users_first_name 		= $_POST['users_first_name'];
+		$users_last_name 		= $_POST['users_last_name'];
+		$users_email 			= $_POST['users_email'];
+		$users_password 		= $_POST['users_password'];
+		$passwordagain  = $_POST['passwordagain'];
+		$md5password 	= md5($users_password);
+		
+		$emptymsg1 = $emptymsg2 = $emptymsg3 = $emptymsg4 = $emptymsg5 = $pasmatchmsg = '';
+		
+		
+		if(empty($users_first_name)){
+			$emptymsg1 = 'Write Firstname';
+		}
+		if(empty($users_last_name)){
+			$emptymsg2 = 'Write Lastname';
+		}
+		if(empty($users_email)){
+			$emptymsg3 = 'Write email';
+		}
+		if(empty($users_password)){
+			$emptymsg4 = 'Write password';
+		}
+		if(empty($passwordagain)){
+			$emptymsg5 = 'Write password Again';
+		}		
+		
+		if(!empty($users_first_name) && !empty($users_last_name) && !empty($users_email) && !empty($users_password) && !empty($passwordagain)){
+			if($users_password !== $passwordagain){
+				$pasmatchmsg = 'Password does not match!';
+			}else{
+				$pasmatchmsg='';
+				$sql = "INSERT INTO users(users_first_name, users_last_name, users_email, users_password) 
+						VALUES('$users_first_name', '$users_last_name', '$users_email', '$md5password')";
+			
+				if($conn->query($sql) == TRUE){
+					header('location:login.php');
+					$_SESSION['signupmsg']='Sign Up Complete. Please Log in now.';
+				}else{
+					echo 'data not inserted';
+				}
+			}
+		}else{
+			$emptymsg = 'Fill up all fields';
+		}
+		
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<meta charset="utf-8">
+		<style>   
+Body {  
+  font-family: Calibri, Helvetica, sans-serif;  
+  background-color: pink;  
+}  
+button {   
+       background-color: #4CAF50;   
+       width: 100%;  
+        color: orange;   
+        padding: 15px;   
+        margin: 10px 0px;   
+        border: none;   
+        cursor: pointer;   
+         }   
+ form {   
+        border: 3px solid #f1f1f1;   
+    }   
+ input[type=text], input[type=password] {   
+        width: 100%;   
+        margin: 8px 0;  
+        padding: 12px 20px;   
+        display: inline-block;   
+        border: 2px solid green;   
+        box-sizing: border-box;   
+    }  
+ button:hover {   
+        opacity: 0.7;   
+    }   
+  .cancelbtn {   
+        width: auto;   
+        padding: 10px 18px;  
+        margin: 10px 5px;  
+    }   
+        
+     
+ .container {   
+        padding: 25px;   
+        background-color: lightblue;  
+    }   
+</style> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="container" style="margin-top:50px">
+				<h3 class="text-center">Registration System</h3>
+			</div>
+			<div class="container" style="margin-top:50px">
+				<div class="row">
+					<div class="col-sm-4">
+						
+					</div>
+					<div class="col-sm-4">
+						<div class="container bg-light p-4">
+							<form action="" method="POST">
+							<div class="mt-2 pb-2">
+								<label for="firstname">First Name:</label>
+								<input type="name" name="users_first_name" class="form-control" placeholder="Your First Name" value="<?php if(isset($_POST['submit'])){echo $users_first_name; } ?>">
+								<span class="text-danger"><?php if(isset($_POST['submit'])){ echo $emptymsg1; }?></span>
+							</div>
+							<div class="mt-2 pb-2">
+								<label for="users_last_name">Last Name:</label>
+								<input type="name" name="users_last_name" class="form-control" placeholder="Your Last Name" value="<?php if(isset($_POST['submit'])){echo $lastname; } ?>">
+								<span class="text-danger"><?php if(isset($_POST['submit'])){ echo $emptymsg2; }?></span>
+							</div>
+							<div class="mt-2 pb-2">
+								<label for="email">Email:</label>
+								<input type="email" name="users_email" class="form-control" placeholder="Enter your email" value="<?php if(isset($_POST['submit'])){echo $email; } ?>">
+								<span class="text-danger"><?php if(isset($_POST['submit'])){ echo $emptymsg3; }?></span>
+							</div>
+							<div class="mt-1 pb-2">
+								<label for="password">Password:</label>
+								<input type="password" name="users_password" class="form-control" placeholder="Enter New password" >
+								<span class="text-danger"><?php if(isset($_POST['submit'])){ echo $emptymsg4; }?></span>
+							</div>
+							<div class="mt-1 pb-2">
+								<label for="password">Password Again:</label>
+								<input type="password" name="passwordagain" class="form-control" placeholder="Enter password Again" >
+								<span class="text-danger"><?php if(isset($_POST['submit'])){ echo $emptymsg5.''.$pasmatchmsg; }?></span>
+							</div>
+							<div class="mt-1 pb-2">
+								<button name="submit" class="btn btn-success">Sign Up</button>
+							</div>
+							<!-- <div class="mt-1 pb-2">
+								Alrady have an account? <a href="login.php" class="text-decoration-none">Login</a>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-sm-4">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
